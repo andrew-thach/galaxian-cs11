e108445fe59d82fb4d20b4eb36bf277415f9175b
Andrew Thach <andrew.thach123@gmail.com>
2025-04-19 15:38:51 -0700

    Generalized Player to an Entity class.

--- Galaxian.cpp
+++ Galaxian.cpp
@@ -5,10 +5,17 @@ Galaxian::Galaxian() {
 }
 
 void Galaxian::play() {
-    initscr();
-    noecho();
-    keypad(stdscr, TRUE);
-    curs_set(FALSE);
+    setlocale(LC_ALL, ""); // Enable UTF-8 support for emojis.
+
+    initscr();             // Initializes ncurses library.
+    keypad(stdscr, TRUE);  // Enable keyboard input.
+    noecho();              // Disable output of keypresses.
+    curs_set(FALSE);       // Disable blinking cursor.
+
+    // Player starts in the bottom center.
+    // COLS and LINES are global variables from ncurses.
+    Player.setX(COLS / 2);
+    Player.setY(LINES - 2);
 
     while(1) {
         draw_game();
@@ -22,8 +29,10 @@ void Galaxian::draw_game() {
     // clear();
 
     mvprintw(0, 0, "Score: %d", score);
+
     // Draw player
-    mvprintw(LINES - 2, COLS / 2, "O");
+    mvprintw(Player.getY(), Player.getX(), "🤖");
+
 
     refresh();
 }
--- Galaxian.h
+++ Galaxian.h
@@ -1,13 +1,30 @@
-#include <ncurses.h>
-#include <unistd.h>
+#include <locale.h>  // Used for UTF-8 support.
+#include <ncurses.h> // Gives us a text based interface.
+#include <unistd.h>  // Used for delaying the refresh rate (TODO: Consider using <chrono> instead)
 
 #define DELAY 100000
+
+class Entity {
+    public:
+        int getX() const {return x;}
+        int getY() const {return y;};
+
+        void setX(int newX) {x = newX;}
+        void setY(int newY) {y = newY;}
+
+    private:
+        int x;
+        int y;
+};
 
 class Galaxian {
     public:
         Galaxian();
         void play();
+
     private:
         void draw_game();
+
+        Entity Player;
         int score;
 };
