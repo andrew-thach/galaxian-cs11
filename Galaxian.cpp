cdbefd64cbee645ce2e483549efa7ba9df2eef30
Andrew Thach <andrew.thach123@gmail.com>
2025-04-19 17:28:48 -0700

    Added feature for difficulty. Initialized enemy entities.

--- Galaxian.cpp
+++ Galaxian.cpp
@@ -1,7 +1,8 @@
 #include "Galaxian.h"
 
-Galaxian::Galaxian() {
+Galaxian::Galaxian(int difficulty) {
     score = 0;
+    num_enemy_rows = difficulty;
 }
 
 void Galaxian::play() {
@@ -12,10 +13,7 @@ void Galaxian::play() {
     noecho();              // Disable output of keypresses.
     curs_set(FALSE);       // Disable blinking cursor.
 
-    // Player starts in the bottom center.
-    // COLS and LINES are global variables from ncurses.
-    Player.setX(COLS / 2);
-    Player.setY(LINES - 2);
+    initialize_entities();
 
     while(1) {
         draw_game();
@@ -25,13 +23,55 @@ void Galaxian::play() {
     endwin();
 }
 
+void Galaxian::initialize_entities() {
+    // Player starts in the bottom center.
+    // COLS and LINES are global variables from ncurses.
+    Player.setX(COLS / 2);
+    Player.setY(LINES - 2);
+
+    const int WAVE_COL_SPACING = 3; // Each enemy is 3 characters apart (horizontally).
+    const int WAVE_ROW_SPACING = 2; // Each enemy is 2 characters apart (vertically).
+
+    // Not necessary, but these variables give room for a future upgrade, 
+    // where one can adjust these variables and make the swarm move.
+    int swarm_row_offset = 1; // Swarm starts below the first line.
+    int swarm_col_offset = 0; // Swarm starts on the left.
+
+    // Initialize enemy grid (pyramid shaped).
+    for(int row_pos = 0; row_pos < num_enemy_rows; row_pos++) {
+        std::vector<Entity> Wave;
+
+        for(int col_pos = 0; col_pos < row_pos * 2 + 1; col_pos++) {
+            // Calculates the horionztal offset for the pyramid row.
+            int pyramid_offset = num_enemy_rows - row_pos - 1;
+
+            // Constructs an enemy with given coordinates, then stores the enemy in the wave.
+            Entity Enemy(
+                    (col_pos + swarm_col_offset + pyramid_offset) * WAVE_COL_SPACING,
+                    (row_pos + swarm_row_offset) * WAVE_ROW_SPACING
+            );
+            Wave.push_back(Enemy);
+        }
+
+        Waves.push_back(Wave); // Add a new row (wave).
+    }
+
+}
+
 void Galaxian::draw_game() {
     // clear();
 
     mvprintw(0, 0, "Score: %d", score);
 
-    // Draw player
+    // Draw player.
     mvprintw(Player.getY(), Player.getX(), "🤖");
+
+    // Draw enemies.
+    for(unsigned i = 0; i < Waves.size(); i++) {
+        for(unsigned j = 0; j < Waves[i].size(); j++) {
+            mvprintw(Waves[i][j].getY(), Waves[i][j].getX(), "👾");
+        }
+    }
 
 
     refresh();
--- Galaxian.h
+++ Galaxian.h
@@ -1,13 +1,18 @@
 #include <locale.h>  // Used for UTF-8 support.
 #include <ncurses.h> // Gives us a text based interface.
 #include <unistd.h>  // Used for delaying the refresh rate (TODO: Consider using <chrono> instead)
+#include <vector>
 
 #define DELAY 100000
 
+// These function prototypes are implemented inline.
 class Entity {
     public:
+        Entity() {x = 0; y = 0;}
+        Entity(int newX, int newY) {x = newX; y = newY;}
+
         int getX() const {return x;}
-        int getY() const {return y;};
+        int getY() const {return y;}
 
         void setX(int newX) {x = newX;}
         void setY(int newY) {y = newY;}
@@ -17,14 +22,19 @@ class Entity {
         int y;
 };
 
+// These function prototypes are implemented in the Galaxian.cpp file.
 class Galaxian {
     public:
-        Galaxian();
+        Galaxian(int difficulty);
         void play();
 
     private:
-        void draw_game();
+        int score;
+        int num_enemy_rows;
 
         Entity Player;
-        int score;
+        std::vector<std::vector<Entity>> Waves;
+
+        void initialize_entities();
+        void draw_game();
 };
