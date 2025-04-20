b5a2f090d3178d6fdb2cf4314d74dee1a02d52dd
Andrew Thach <andrew.thach123@gmail.com>
2025-04-19 19:32:20 -0700

    Give user a difficulty choice, and print out final score upon game termination.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include "Galaxian.h"
 
 int main() {
-    int difficulty = 3;
+    
+    int difficulty;
+    std::cout << "What difficulty do you want (1 - 5)? ";
+    std::cin >> difficulty;
     Galaxian Round(difficulty);
+
     Round.play();
+
+    int final_score = Round.getScore();
+    std::cout << "Game over!" << std::endl;
+    std::cout << "Final Score: " << final_score << std::endl;
 }
